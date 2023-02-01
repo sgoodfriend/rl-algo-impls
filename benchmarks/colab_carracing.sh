28a89a07b9592e03cf3130fefe13ef48509dd7cd
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-01-31 23:22:17 -0800

    colab benchmarking scripts push to rl-algo-impls-benchmarks project

--- benchmarks/colab_carracing.sh
+++ benchmarks/colab_carracing.sh
@@ -1,10 +1,11 @@
 ALGOS="ppo"
 ENVS="CarRacing-v0"
 SEEDS="1 2 3"
+WANDB_TAGS="benchmark_$(git rev-parse --short HEAD) host_$(hostname)"
 for algo in $ALGOS; do
     for env in $ENVS; do
         for seed in $SEEDS; do
-            python train.py --algo $algo --env $env --seed $seed --pool-size 1
+            python train.py --algo $algo --env $env --seed $seed --pool-size 1 --wandb-tags $WANDB_TAGS --wandb-project-name rl-algo-impls-benchmarks
         done
     done
 done
