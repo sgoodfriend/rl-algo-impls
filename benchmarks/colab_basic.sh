7e4a9b996f7c6fbaa2c6ce8861a27ad88aa9039e
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-02-01 10:31:30 -0800

    hypens instead of underscores is so unnatural

--- benchmarks/colab_basic.sh
+++ benchmarks/colab_basic.sh
@@ -1,4 +1,4 @@
 source benchmarks/train_loop.sh
 ALGOS="ppo"
 ENVS="CartPole-v1 MountainCar-v0 MountainCarContinuous-v0 Acrobot-v1 LunarLander-v2"
-train_loop $ALGOS "$ENVS" | xargs -I CMD --max_procs=3 bash -c CMD
+train_loop $ALGOS "$ENVS" | xargs -I CMD --max-procs=3 bash -c CMD
