edb89269b66b0489d18fbe8b0490eeded0108c55
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-02-02 01:58:31 +0000

    Activate VIRTUAL_DISPLAY for lambda benchmarking

--- lambda_labs/benchmark.sh
+++ lambda_labs/benchmark.sh
@@ -1,5 +1,6 @@
 # export BENCHMARK_MAX_PROCS=1
 # export WANDB_PROJECT_NAME="rl-algo-impls"
+export VIRTUAL_DISPLAY=1
 ./benchmarks/colab_basic.sh
 ./benchmarks/colab_pybullet.sh
 ./benchmarks/colab_carracing.sh
