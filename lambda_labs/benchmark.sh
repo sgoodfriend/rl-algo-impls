8a83f34a3232e2d016fdcc460f4eee1bf1df3533
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-02-03 10:54:02 -0800

    Monolithic lambda labs benchmark file

--- lambda_labs/benchmark.sh
+++ lambda_labs/benchmark.sh
@@ -1,8 +1,33 @@
-# export BENCHMARK_MAX_PROCS=1
+source benchmarks/train_loop.sh
+
 # export WANDB_PROJECT_NAME="rl-algo-impls"
 export VIRTUAL_DISPLAY=1
-./benchmarks/colab_basic.sh
-./benchmarks/colab_pybullet.sh
-./benchmarks/colab_carracing.sh
-./benchmarks/colab_atari1.sh
-./benchmarks/colab_atari2.sh
+
+BENCHMARK_MAX_PROCS="${BENCHMARK_MAX_PROCS:-6}"
+
+ALGOS=(
+    "vpg"
+    "dqn"
+    "ppo"
+)
+ENVS=(
+    # Basic
+    "CartPole-v1"
+    "MountainCar-v0"
+    "MountainCarContinuous-v0"
+    "Acrobot-v1"
+    "LunarLander-v2"
+    # PyBullet
+    "HalfCheetahBulletEnv-v0"
+    "AntBulletEnv-v0"
+    "Walker2DBulletEnv-v0"
+    "HopperBulletEnv-v0"
+    # CarRacing
+    "CarRacing-v0"
+    # Atari
+    "PongNoFrameskip-v4"
+    "BreakoutNoFrameskip-v4"
+    "SpaceInvadersNoFrameskip-v4"
+    "QbertNoFrameskip-v4"
+)
+train_loop "${ALGOS[*]}" "${ENVS[*]}" | xargs -I CMD -P $BENCHMARK_MAX_PROCS bash -c CMD
