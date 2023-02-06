5598ebc4b03054f16eebe76792486ba7bcacfc5c
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-02-05 17:15:59 -0800

    SEEDS is environment variable to allow benchmarking on different seeds

--- benchmarks/train_loop.sh
+++ benchmarks/train_loop.sh
@@ -6,9 +6,10 @@ train_loop () {
     local WANDB_PROJECT_NAME="${WANDB_PROJECT_NAME:-rl-algo-impls-benchmarks}"
     local args=()
     (( VIRTUAL_DISPLAY == 1)) && args+=("--virtual-display")
+    local SEEDS="${SEEDS:-1 2 3}"
     for algo in $(echo $1); do
         for env in $(echo $2); do
-            for seed in {1..3}; do
+            for seed in $SEEDS; do
                 echo python train.py --algo $algo --env $env --seed $seed --pool-size 1 --wandb-tags $WANDB_TAGS --wandb-project-name $WANDB_PROJECT_NAME ${args[@]}
             done
         done
