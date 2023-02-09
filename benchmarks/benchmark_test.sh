eb4fdd315369652f9c5b84dbff4f1416d4b9faac
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-02-08 16:39:36 -0800

    Remove mentions of virtual_display as an argument or env variable

--- benchmarks/benchmark_test.sh
+++ benchmarks/benchmark_test.sh
@@ -1,7 +1,6 @@
 source benchmarks/train_loop.sh
 
 export WANDB_PROJECT_NAME="rl-algo-impls"
-export VIRTUAL_DISPLAY=1
 
 BENCHMARK_MAX_PROCS="${BENCHMARK_MAX_PROCS:-3}"
 
--- benchmarks/train_loop.sh
+++ benchmarks/train_loop.sh
@@ -4,13 +4,11 @@ train_loop () {
     local env
     local seed
     local WANDB_PROJECT_NAME="${WANDB_PROJECT_NAME:-rl-algo-impls-benchmarks}"
-    local args=()
-    (( VIRTUAL_DISPLAY == 1)) && args+=("--virtual-display")
     local SEEDS="${SEEDS:-1 2 3}"
     for algo in $(echo $1); do
         for env in $(echo $2); do
             for seed in $SEEDS; do
-                echo python train.py --algo $algo --env $env --seed $seed --pool-size 1 --wandb-tags $WANDB_TAGS --wandb-project-name $WANDB_PROJECT_NAME ${args[@]}
+                echo python train.py --algo $algo --env $env --seed $seed --pool-size 1 --wandb-tags $WANDB_TAGS --wandb-project-name $WANDB_PROJECT_NAME
             done
         done
     done
