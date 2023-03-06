fbdf1716c053d1995e28881c18fed8cff4927117
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-03-06 11:23:01 -0800

    Support restarting tuning run. Seeds increment by 100 starting at 100

--- tuning/tuning.sh
+++ tuning/tuning.sh
@@ -1,19 +1,25 @@
-ALGO=$1
-ENV=$2
-N_JOBS=$3
+while getopts a:e:n:s: flag
+do
+    case "${flag}" in
+        a) algo=${OPTARG};;
+        e) env=${OPTARG};;
+        n) n_jobs=${OPTARG};;
+        s) study_name=${OPTARG};;
+    esac
+done
 
 TZ="America/Los_Angeles"
 NOW=$(date +"%Y-%m-%dT%H:%M:%S")
-STUDY_NAME="$ALGO-$ENV-$NOW"
+study_name="${study_name:-$algo-$env-$NOW}"
 STORAGE_PATH="sqlite:///runs/tuning.db"
 
 mkdir -p runs
-optuna create-study --study-name $STUDY_NAME --storage $STORAGE_PATH --direction maximize
+optuna create-study --study-name $study_name --storage $STORAGE_PATH --direction maximize --skip-if-exists
 
 optimize () {
-    for ((s=1;s<=N_JOBS;s++)); do
-        echo python optimize.py --algo $ALGO --env $ENV --seed $s --load-study --study-name $STUDY_NAME --storage-path $STORAGE_PATH
+    for ((s=100;s<=n_jobs*100;s+=100)); do
+        echo python optimize.py --algo $algo --env $env --seed $s --load-study --study-name $study_name --storage-path $STORAGE_PATH
     done
 }
 
-optimize | xargs -I CMD -P $N_JOBS bash -c CMD
+optimize | xargs -I CMD -P $n_jobs bash -c CMD
