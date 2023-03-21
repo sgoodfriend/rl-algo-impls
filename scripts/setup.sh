db0ff60ba9ae6d4ae1f089f58fd00e8f7dd4734a
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-03-20 18:17:46 -0700

    Add java to setup.sh for microrts support

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,7 @@ sudo apt install -y python-opengl
 sudo apt install -y ffmpeg
 sudo apt install -y xvfb
 sudo apt install -y swig
+sudo apt install -y default-jdk
 
 python3 -m pip install --upgrade pip
 pip install --upgrade torch torchvision torchaudio
