sgoodfriend/rl-algo-impls

commit cd7969edf121e71b9bcad4a92dc3c17fe547eadf

benchmark script supports multiple algos

rl_algo_impls/publish/markdown_format.py
@@ -163,7 +163,7 @@ git clone git@github.com:sgoodfriend/rl-algo-impls.git
 cd rl-algo-impls
 bash ./lambda_labs/setup.sh
 wandb login
-bash ./lambda_labs/benchmark.sh
+bash ./lambda_labs/benchmark.sh [-a {"ppo"}] [-e ENVS] [-j {6}] [-p {rl-algo-impls-benchmarks}] [-s {"1 2 3"}]
 ```
 
 ### Alternative: Google Colab Pro+

scripts/benchmark.sh
@@ -1,7 +1,7 @@
 while test $# != 0
 do
     case "$1" in
-        -a) algo=$2 ;;
+        -a) algos=$2 ;;
         -j) n_jobs=$2 ;;
         -p) project_name=$2 ;;
         -s) seeds=$2 ;;
@@ -11,18 +11,26 @@ do
     shift
 done
 
-algo="${algo:-ppo}"
+algos="${algos:-ppo}"
 n_jobs="${n_jobs:-6}"
 project_name="${project_name:-rl-algo-impls-benchmarks}"
 seeds="${seeds:-1 2 3}"
 
-BENCHMARK_ENVS=(
+DISCRETE_ENVS=(
     # Basic
     "CartPole-v1"
     "MountainCar-v0"
-    "MountainCarContinuous-v0"
     "Acrobot-v1"
     "LunarLander-v2"
+    # Atari
+    "PongNoFrameskip-v4"
+    "BreakoutNoFrameskip-v4"
+    "SpaceInvadersNoFrameskip-v4"
+    "QbertNoFrameskip-v4"
+)
+BOX_ENVS=(
+    # Basic
+    "MountainCarContinuous-v0"
     "BipedalWalker-v3"
     # PyBullet
     "HalfCheetahBulletEnv-v0"
@@ -31,25 +39,29 @@ BENCHMARK_ENVS=(
     "Walker2DBulletEnv-v0"
     # CarRacing
     "CarRacing-v0"
-    # Atari
-    "PongNoFrameskip-v4"
-    "BreakoutNoFrameskip-v4"
-    "SpaceInvadersNoFrameskip-v4"
-    "QbertNoFrameskip-v4"
 )
-if [ -z $envs ]; then
-    echo "-e unspecified; therefore, benchmark training on ${BENCHMARK_ENVS[*]}"
-    envs=${BENCHMARK_ENVS[*]}
-fi
 
-PROCGEN_ENVS=(
-    "procgen-coinrun-easy"
-    "procgen-starpilot-easy"
-    "procgen-bossfight-easy"
-    "procgen-bigfish-easy"
-)
-if [ "$procgen" = "t" ]; then
-    envs=${PROCGEN_ENVS[*]}
-fi
+for algo in $(echo $algos); do
+    if [ "$algo" = "dqn" ]; then
+        BENCHMARK_ENVS="${DISCRETE_ENVS[*]}"
+    else
+        BENCHMARK_ENVS="${DISCRETE_ENVS[*]} ${BOX_ENVS[*]}"
+    fi
+    algo_envs=$envs
+    if [ -z $algo_envs ]; then
+        echo "-e unspecified; therefore, benchmark training on ${BENCHMARK_ENVS[*]}"
+        algo_envs=${BENCHMARK_ENVS[*]}
+    fi
 
-bash scripts/train_loop.sh -a $algo -e "$envs" -p $project_name -s "$seeds" # xargs -I CMD -P $n_jobs bash -c CMD
+    PROCGEN_ENVS=(
+        "procgen-coinrun-easy"
+        "procgen-starpilot-easy"
+        "procgen-bossfight-easy"
+        "procgen-bigfish-easy"
+    )
+    if [ "$procgen" = "t" ]; then
+        algo_envs=${PROCGEN_ENVS[*]}
+    fi
+
+    bash scripts/train_loop.sh -a $algo -e "$algo_envs" -p $project_name -s "$seeds" | xargs -I CMD -P $n_jobs bash -c CMD
+done

commit 788f4f65f03b49c06ad23855c5647f8576d8344d

Fix formatting error
Introduced by https://github.com/sgoodfriend/rl-algo-impls/commit/cd7969edf121e71b9bcad4a92dc3c17fe547eadf#diff-ce6c0372909568bfb3dd07f9b893b1a64f157cf647938f9b62324ca79bae45ff

rl_algo_impls/publish/markdown_format.py
@@ -163,7 +163,7 @@ git clone git@github.com:sgoodfriend/rl-algo-impls.git
 cd rl-algo-impls
 bash ./lambda_labs/setup.sh
 wandb login
-bash ./lambda_labs/benchmark.sh [-a {"ppo"}] [-e ENVS] [-j {6}] [-p {rl-algo-impls-benchmarks}] [-s {"1 2 3"}]
+bash ./lambda_labs/benchmark.sh [-a {{"ppo a2c dqn vpg"}}] [-e ENVS] [-j {{6}}] [-p {{rl-algo-impls-benchmarks}}] [-s {{"1 2 3"}}]
 ```
 
 ### Alternative: Google Colab Pro+