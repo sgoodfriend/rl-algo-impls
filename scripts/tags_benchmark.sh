bf9db083b7e529179d1ed24befa5653e69daa048
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-03-30 17:35:47 -0700

    benchmark tags include branch and version number

--- scripts/tags_benchmark.sh
+++ scripts/tags_benchmark.sh
@@ -1,1 +1,5 @@
-echo "benchmark_$(git rev-parse --short HEAD) host_$(hostname)"
+commit="benchmark_$(git rev-parse --short HEAD)"
+host="host_$(hostname)"
+branch="branch_$(git rev-parse --abbrev-ref HEAD)"
+version="v$(pip show rl_algo_impls | grep Version | sed -e 's#Version:\ ##')"
+echo "$commit $host $branch $version"
