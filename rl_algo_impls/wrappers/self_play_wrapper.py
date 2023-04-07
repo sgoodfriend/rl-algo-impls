1125675b74be2ec6322f6ae30160c796f8f45e7d
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-04-07 15:45:09 -0700

    swap_window_size forces historical policy batching
    This will hopefully sagnificantly improve performance

--- rl_algo_impls/wrappers/self_play_wrapper.py
+++ rl_algo_impls/wrappers/self_play_wrapper.py
@@ -25,11 +25,18 @@ class SelfPlayWrapper(VecotarableWrapper):
         save_steps: int = 20_000,
         swap_steps: int = 10_000,
         window: int = 10,
+        swap_window_size: int = 2,
     ) -> None:
         super().__init__(env)
+        assert num_old_policies % 2 == 0, f"num_old_policies must be even"
+        assert (
+            num_old_policies % swap_window_size == 0
+        ), f"num_old_policies must be a multiple of swap_window_size"
+
         self.num_old_policies = num_old_policies
         self.save_steps = save_steps
         self.swap_steps = swap_steps
+        self.swap_window_size = swap_window_size
 
         self.policies: Deque[Policy] = deque(maxlen=window)
         self.policy_assignments: List[Optional[Policy]] = [None] * env.num_envs
@@ -52,16 +59,15 @@ class SelfPlayWrapper(VecotarableWrapper):
                 # Switch between player 1 and 2
                 self.policy_assignments[2 * i + (i % 2)] = copied_policy
 
-    def swap_policy(self, idx: int) -> None:
+    def swap_policy(self, idx: int, swap_window_size: int = 1) -> None:
         policy = random.choice(self.policies)
         idx = idx // 2 * 2
-        if self.policy_assignments[idx] is not None:
-            # Switch policy from player 1 to player 2
-            assignment = [None, policy]
-        else:
-            assignment = [policy, None]
-        self.policy_assignments[idx : idx + 2] = assignment
-        self.steps_since_swap[idx : idx + 2] = [0, 0]
+        for j in range(swap_window_size * 2):
+            if self.policy_assignments[idx + j]:
+                self.policy_assignments[idx + j] = policy
+        self.steps_since_swap[idx : idx + swap_window_size * 2] = np.zeros(
+            swap_window_size * 2
+        )
 
     def step(self, actions: np.ndarray) -> VecEnvStepReturn:
         env = self.env  # type: ignore
@@ -69,7 +75,7 @@ class SelfPlayWrapper(VecotarableWrapper):
         orig_learner_indexes = self.learner_indexes()
 
         all_actions[orig_learner_indexes] = actions
-        for policy in self.policies:
+        for policy in set(p for p in self.policy_assignments if p):
             policy_indexes = [policy == p for p in self.policy_assignments]
             if any(policy_indexes):
                 all_actions[policy_indexes] = policy.act(
@@ -86,9 +92,9 @@ class SelfPlayWrapper(VecotarableWrapper):
         info = [i for i, b in zip(info, orig_learner_indexes) if b]
 
         self.steps_since_swap += 1
-        for idx in range(0, self.num_old_policies * 2, 2):
-            if done[idx] and self.steps_since_swap[idx] > self.swap_steps:
-                self.swap_policy(idx)
+        for idx in range(0, self.num_old_policies * 2, 2 * self.swap_window_size):
+            if self.steps_since_swap[idx] > self.swap_steps:
+                self.swap_policy(idx, self.swap_window_size)
 
         new_learner_indexes = self.learner_indexes()
         return self.next_obs[new_learner_indexes], rew, done[new_learner_indexes], info
