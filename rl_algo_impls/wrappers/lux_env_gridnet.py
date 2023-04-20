68196a20deb4eccbdbe2e3156a1a47f743b785cb
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-04-20 11:02:49 -0500

    No reward for building robots. Higher reward for water generation
    4 points for a day of factory water seems like a lot, but got to get the
    model to last more than 150 turns.

--- rl_algo_impls/wrappers/lux_env_gridnet.py
+++ rl_algo_impls/wrappers/lux_env_gridnet.py
@@ -44,13 +44,13 @@ LICHEN_FACTORY_MAX = 128_000
 DEFAULT_REWARD_WEIGHTS = (
     10,  # WIN_LOSS
     0.001,  # LICHEN_DELTA (clip to +/- 1)
-    5e-3,  # ICE_GENERATION (1 for a day of water for factory, 0.1 for a heavy dig action)
+    0.01,  # ICE_GENERATION (2 for a day of water for factory, 0.2 for a heavy dig action)
     2e-3,  # ORE_GENERATION (1 for building a heavy robot, 0.04 for a heavy dig action)
-    0.02,  # WATER_GENERATION (1 for a day of water for factory)
+    0.04,  # WATER_GENERATION (2 for a day of water for factory)
     0.01,  # METAL_GENERATION (1 for building a heavy robot
     0,  # LICHEN_GENERATION (0)
-    0.04,  # BUILT_LIGHT
-    0.6,  # BUILT_HEAVY
+    0,  # BUILT_LIGHT
+    0,  # BUILT_HEAVY
 )
 
 
