9e009aa6d7de1593c52bd6f9165b62ad76f114f7
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-04-24 16:02:59 -0700

    Refactor/move main.py to be a single function

--- lux.py
+++
@@ -1,4 +0,0 @@
-from rl_algo_impls.lux.main import run_loop
-
-if __name__ == "__main__":
-    run_loop()
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import json
 from argparse import Namespace
 
-import numpy as np
-
 from rl_algo_impls.lux.agent import Agent
 from rl_algo_impls.lux.kit.config import EnvConfig
+from rl_algo_impls.lux.kit.kit import process_action, process_obs
 
 ### DO NOT REMOVE THE FOLLOWING CODE ###
 agent_dict = (
@@ -44,64 +43,8 @@ def agent_fn(observation, configurations):
     return process_action(actions)
 
 
-def process_action(action):
-    return to_json(action)
+if __name__ == "__main__":
 
-
-def to_json(obj):
-    if isinstance(obj, np.ndarray):
-        return obj.tolist()
-    elif isinstance(obj, np.integer):
-        return int(obj)
-    elif isinstance(obj, np.floating):
-        return float(obj)
-    elif isinstance(obj, list) or isinstance(obj, tuple):
-        return [to_json(s) for s in obj]
-    elif isinstance(obj, dict):
-        out = {}
-        for k in obj:
-            out[k] = to_json(obj[k])
-        return out
-    else:
-        return obj
-
-
-def from_json(state):
-    if isinstance(state, list):
-        return np.array(state)
-    elif isinstance(state, dict):
-        out = {}
-        for k in state:
-            out[k] = from_json(state[k])
-        return out
-    else:
-        return state
-
-
-def process_obs(player, game_state, step, obs):
-    if step == 0:
-        # at step 0 we get the entire map information
-        game_state = from_json(obs)
-    else:
-        # use delta changes to board to update game state
-        obs = from_json(obs)
-        for k in obs:
-            if k != "board":
-                game_state[k] = obs[k]
-            else:
-                if "valid_spawns_mask" in obs[k]:
-                    game_state["board"]["valid_spawns_mask"] = obs[k][
-                        "valid_spawns_mask"
-                    ]
-        for item in ["rubble", "lichen", "lichen_strains"]:
-            for k, v in obs["board"][item].items():
-                k = k.split(",")
-                x, y = int(k[0]), int(k[1])
-                game_state["board"][item][x, y] = v
-    return game_state
-
-
-def run_loop():
     def read_input():
         """
         Reads input from stdin
@@ -134,7 +77,3 @@ def run_loop():
         actions = agent_fn(observation, dict(env_cfg=configurations))
         # send actions to engine
         print(json.dumps(actions))
-
-
-if __name__ == "__main__":
-    run_loop()
