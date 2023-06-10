29ac35e7ec9080050f557b86106eadd70413df18
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-06-09 21:03:52 -0700

    Revert "Back to Ubuntu default Java"
    This reverts commit 082c7c7ed31ee2dbb72b78601ccf9460a66b4e12.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,11 @@ sudo apt install -y xvfb
 sudo apt install -y swig
 sudo apt install -y default-jdk
 
+wget https://download.oracle.com/java/19/archive/jdk-19.0.2_linux-x64_bin.deb
+sudo apt-get -qqy install ./jdk-19.0.2_linux-x64_bin.deb
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk-19/bin/java 1919
+rm jdk-19.0.2_linux-x64_bin.deb
+
 python3 -m pip install --upgrade pip
 pip install --upgrade torch torchvision torchaudio
 
