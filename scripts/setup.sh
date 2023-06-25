c6977cab45b224f9670e4b6bb91f31eceee24b7b
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-06-24 22:26:21 -0700

    Add git-lfs init to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,10 @@ sudo apt install -y xvfb
 sudo apt install -y swig
 sudo apt install -y default-jdk
 
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt install -y git-lfs
+git lfs pull
+
 python3 -m pip install --upgrade pip
 pip install --upgrade torch torchvision torchaudio
 
