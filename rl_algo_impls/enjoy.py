c54c5d4c0a09019dea38552a321cb2adabaf9e74
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-07-13 18:26:55 -0700

    Don’t create tensorboard_folder by default in enjoy.py

--- rl_algo_impls/enjoy.py
+++ rl_algo_impls/enjoy.py
@@ -35,7 +35,7 @@ def enjoy() -> None:
         render=False,
         override_hparams='{"bots":{"mayari":1}}',
         video_path=os.path.expanduser("~/Desktop/NoWhereToRun-RAISocketAI-Mayari"),
-        tensorboard_folder="visualize_model",
+        # tensorboard_folder="visualize_model",
         thop=True,
     )
     args = parser.parse_args()
