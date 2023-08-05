7e32276046689597938df4c03e173a55310d4f67
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-08-05 00:51:59 -0700

    Update TrainStats to handle numpy arrays

--- rl_algo_impls/acbc/train_stats.py
+++ rl_algo_impls/acbc/train_stats.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 
 import numpy as np
 from torch.utils.tensorboard.writer import SummaryWriter
@@ -6,18 +6,35 @@ from torch.utils.tensorboard.writer import SummaryWriter
 
 class TrainStats:
     def __init__(
-        self, step_stats: List[Dict[str, float]], explained_var: float
+        self,
+        step_stats: List[Dict[str, Union[float, np.ndarray]]],
+        explained_var: float,
     ) -> None:
-        self.step_stats = {
-            k: np.mean([s[k] for s in step_stats]).item() for k in step_stats[0]
-        }
+        self.step_stats: Dict[str, Union[float, np.ndarray]] = {}
+        for k, item in step_stats[0].items():
+            if isinstance(item, np.ndarray):
+                self.step_stats[k] = np.mean([s[k] for s in step_stats], axis=0)
+            else:
+                self.step_stats[k] = np.mean([s[k] for s in step_stats]).item()
         self.explained_var = explained_var
 
     def write_to_tensorboard(self, tb_writer: SummaryWriter, global_step: int) -> None:
         stats = {**self.step_stats, **{"explained_var": self.explained_var}}
         for name, value in stats.items():
-            tb_writer.add_scalar(f"losses/{name}", value, global_step=global_step)
+            if isinstance(value, np.ndarray):
+                for idx, v in enumerate(value.flatten()):
+                    tb_writer.add_scalar(
+                        f"losses/{name}_{idx}", v, global_step=global_step
+                    )
+            else:
+                tb_writer.add_scalar(f"losses/{name}", value, global_step=global_step)
 
     def __repr__(self) -> str:
         stats = {**self.step_stats, **{"explained_var": self.explained_var}}
-        return " | ".join(f"{k}: {round(v, 2)}" for k, v in stats.items())
+        s = []
+        for name, value in stats.items():
+            if isinstance(value, np.ndarray):
+                s.append(f"{name}: " + ",".join(round(v, 2) for v in value.flatten()))
+            else:
+                s.append(f"{name}: {round(value, 2)}")
+        return " | ".join(s)
