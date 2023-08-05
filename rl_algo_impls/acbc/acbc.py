6419a552e2789159022be8b33a119fc712ff2631
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-08-05 00:46:41 -0700

    ACBC supports vector gamma, gae_lambda, vf_coef

--- rl_algo_impls/acbc/acbc.py
+++ rl_algo_impls/acbc/acbc.py
@@ -1,7 +1,7 @@
 import logging
 from dataclasses import astuple
 from time import perf_counter
-from typing import Dict, List, Optional, TypeVar
+from typing import Dict, List, Optional, TypeVar, Union
 
 import numpy as np
 import torch
@@ -10,12 +10,14 @@ from torch.optim import Adam, Optimizer
 from torch.utils.tensorboard.writer import SummaryWriter
 
 from rl_algo_impls.acbc.train_stats import TrainStats
+from rl_algo_impls.ppo.ppo import NL
 from rl_algo_impls.ppo.rollout import RolloutGenerator
 from rl_algo_impls.shared.algorithm import Algorithm
 from rl_algo_impls.shared.callbacks.callback import Callback
 from rl_algo_impls.shared.policy.actor_critic import ActorCritic
 from rl_algo_impls.shared.schedule import schedule, update_learning_rate
 from rl_algo_impls.shared.stats import log_scalars
+from rl_algo_impls.shared.tensor_utils import num_or_array
 
 """
 Actor-Critic Behavior Cloning with Critic Bootstrapping
@@ -34,9 +36,9 @@ class ACBC(Algorithm):
         learning_rate_decay: str = "none",
         batch_size: int = 64,
         n_epochs: int = 10,
-        gamma: float = 0.99,
-        gae_lambda: float = 0.95,
-        vf_coef: float = 0.25,
+        gamma: NL = 0.99,
+        gae_lambda: NL = 0.95,
+        vf_coef: NL = 0.25,
         max_grad_norm: float = 0.5,
         gradient_accumulation: bool = False,
         scale_loss_by_num_actions: bool = False,
@@ -47,9 +49,9 @@ class ACBC(Algorithm):
         self.learning_rate_schedule = schedule(learning_rate_decay, learning_rate)
         self.batch_size = batch_size
         self.n_epochs = n_epochs
-        self.gamma = gamma
-        self.gae_lambda = gae_lambda
-        self.vf_coef = vf_coef
+        self.gamma = num_or_array(gamma)
+        self.gae_lambda = num_or_array(gae_lambda)
+        self.vf_coef = num_or_array(vf_coef)
         self.max_grad_norm = max_grad_norm
         self.gradient_accumulation = gradient_accumulation
         self.scale_loss_by_num_actions = scale_loss_by_num_actions
@@ -81,13 +83,15 @@ class ACBC(Algorithm):
 
             chart_scalars = {
                 "learning_rate": optimizer.param_groups[0]["lr"],
+                "vf_coef": self.vf_coef,
             }
             log_scalars(self.tb_writer, "charts", chart_scalars, timesteps_elapsed)
 
             r = rollout_generator.rollout(self.gamma, self.gae_lambda)
             timesteps_elapsed += r.total_steps
 
-            step_stats: List[Dict[str, float]] = []
+            step_stats: List[Dict[str, Union[float, np.ndarray]]] = []
+            vf_coef = torch.Tensor(np.array(self.vf_coef)).to(self.device)
             for e in range(self.n_epochs):
                 step_stats.clear()
                 for mb in r.minibatches(self.batch_size, self.device):
@@ -114,7 +118,7 @@ class ACBC(Algorithm):
                     else:
                         pi_loss = -new_logprobs.mean()
                     v_loss = ((new_values - mb_returns) ** 2).mean(0)
-                    loss = pi_loss + self.vf_coef * v_loss
+                    loss = pi_loss + (vf_coef * v_loss).sum()
 
                     if self.gradient_accumulation:
                         loss /= r.num_minibatches(self.batch_size)
@@ -126,7 +130,7 @@ class ACBC(Algorithm):
                         {
                             "loss": loss.item(),
                             "pi_loss": pi_loss.item(),
-                            "v_loss": v_loss.item(),
+                            "v_loss": v_loss.detach().cpu().numpy(),
                         }
                     )
 
