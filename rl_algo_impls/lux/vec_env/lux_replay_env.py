f8e6ba6517aa8c4b7190f204ff7541319b401db1
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-08-08 10:31:05 -0700

    Refactor factory+unit action with fallback

--- rl_algo_impls/lux/vec_env/lux_replay_env.py
+++ rl_algo_impls/lux/vec_env/lux_replay_env.py
@@ -13,6 +13,8 @@ from rl_algo_impls.lux.actions import (
     UNIT_ACTION_SIZES,
 )
 from rl_algo_impls.lux.kit.kit import GameState
+from rl_algo_impls.lux.kit.unit import Unit
+from rl_algo_impls.lux.kit.utils import my_turn_to_place_factory
 from rl_algo_impls.lux.observation import observation_and_action_mask
 from rl_algo_impls.lux.rewards import MIN_SCORE, LuxRewardWeights
 from rl_algo_impls.lux.shared import pos_to_idx
@@ -94,6 +96,7 @@ class LuxReplayEnv(Env):
             action,
             self._action_mask,
             self._last_game_state,
+            self._last_enqueued_actions,
         )
         obs = self._from_lux_state(state)
 
@@ -129,6 +132,7 @@ class LuxReplayEnv(Env):
     def _from_lux_state(self, lux_state: LuxState) -> np.ndarray:
         (obs_state_dict, game_state, enqueued_actions, player) = lux_state
         self._last_game_state = game_state
+        self._last_enqueued_actions = enqueued_actions
         obs, self._action_mask = observation_and_action_mask(
             player,
             obs_state_dict,
@@ -156,6 +160,7 @@ def from_lux_action(
     lux_action: Dict[str, Union[int, str, List[List[int]]]],
     action_mask: Dict[str, np.ndarray],
     game_state: GameState,
+    enqueued_actions: Dict[str, np.ndarray],
 ) -> Dict[str, np.ndarray]:
     num_map_tiles = len(action_space["per_position"].nvec) // len(ACTION_SIZES)
     map_size = int(np.sqrt(num_map_tiles))
@@ -192,64 +197,96 @@ def from_lux_action(
             game_state.teams[player].place_first, game_state.env_steps
         )
         return action
+    factory_actions = {}
+    unit_actions = {}
+    for u_id in game_state.units[player]:
+        a = enqueued_actions.get(u_id, None)
+        if a is not None:
+            unit_actions[u_id] = np.where(a >= 0, a, 0)
+        else:
+            unit_actions[u_id] = None
     for fu_id, a in lux_action.items():
         if fu_id.startswith("factory"):
             assert isinstance(a, int)
-            factory = game_state.factories[player][fu_id]
-            pos_idx = pos_to_idx(factory.pos, map_size)
-            factory_mask = action_mask["per_position"][
-                pos_idx, :FACTORY_ACTION_ENCODED_SIZE
-            ]
-            if not factory_mask[a]:
-                logging.warn(
-                    f"Invalid factory action {a} for factory {factory}. Mask {factory_mask}"
-                )
-                continue
-            action["per_position"][pos_idx, 0] = a + 1
+            factory_actions[fu_id] = a
         else:
-            unit = game_state.units[player][fu_id]
-            pos_idx = pos_to_idx(unit.pos, map_size)
             assert isinstance(a, list)
             a_type, a_dir, a_resource_type, a_amount, a_repeat, a_n = a[0]
-            pos_mask = action_mask["per_position"][pos_idx]
-            unit_masks = []
-            idx = FACTORY_ACTION_ENCODED_SIZE
-            for mask_sz in UNIT_ACTION_SIZES:
-                unit_masks.append(pos_mask[idx : idx + mask_sz])
-                idx += mask_sz
-            if not unit_masks[0][a_type]:
-                logging.warn(
-                    f"Invalid action type {a_type} for unit {unit}. Mask: {unit_masks[0]}"
-                )
-                continue
             _a = np.zeros(len(UNIT_ACTION_SIZES), dtype=np.int32)
             _a[0] = a_type
             if a_type == 0:
-                if not unit_masks[1][a_dir]:
-                    logging.warn(
-                        f"Invalid move dir {a_dir} for unit {unit}. Mask: {unit_masks[1]}"
-                    )
-                    continue
                 _a[1] = a_dir
             elif a_type == 1:
-                if not unit_masks[2][a_dir]:
-                    logging.warn(
-                        f"Invalid transfer dir {a_dir} for unit {unit}. Mask: {unit_masks[2]}"
-                    )
-                    continue
-                if not unit_masks[3][a_resource_type]:
-                    logging.warn(
-                        f"Invalid transfer resource {a_resource_type} for unit {unit}. Mask: {unit_masks[3]}"
-                    )
-                    continue
                 _a[2] = a_dir
                 _a[3] = a_resource_type
             elif a_type == 2:
-                if not unit_masks[4][a_resource_type]:
-                    logging.warn(
-                        f"Invalid pickup resource {a_resource_type} for unit {unit}. Mask: {unit_masks[4]}"
-                    )
-                    continue
                 _a[4] = a_resource_type
-            action["per_position"][pos_idx, 1:] = _a
+            unit_actions[fu_id] = _a
+    for f_id, a in factory_actions.items():
+        factory = game_state.factories[player][f_id]
+        pos_idx = pos_to_idx(factory.pos, map_size)
+        factory_mask = action_mask["per_position"][
+            pos_idx, :FACTORY_ACTION_ENCODED_SIZE
+        ]
+        if not factory_mask[a + 1]:
+            logging.warn(
+                f"Invalid factory action {a + 1} for factory {factory}. Mask {factory_mask}"
+            )
+            continue
+        action["per_position"][pos_idx, 0] = a + 1
+    for u_id, a in unit_actions.items():
+        unit = game_state.units[player][u_id]
+        pos_idx = pos_to_idx(unit.pos, map_size)
+        pos_mask = action_mask["per_position"][pos_idx]
+        if is_valid_action(a, pos_mask, unit):
+            action["per_position"][pos_idx, 1:] = a
+        elif np.any(pos_mask[FACTORY_ACTION_ENCODED_SIZE:]):
+            fa = np.zeros(len(UNIT_ACTION_SIZES), dtype=np.int32)
+            fa[0] = UNIT_ACTION_SIZES[0] - 1  # Recharge
+            if not is_valid_action(fa, pos_mask, unit):
+                # TODO: Need to use legal value in this case.
+                logging.error(f"Fallback action invalid")
+            action["per_position"][pos_idx, 1:] = fa
     return action
+
+
+def is_valid_action(
+    action: Optional[np.ndarray], pos_mask: np.ndarray, unit: Unit
+) -> bool:
+    if action is None:
+        return False
+    m_idx = FACTORY_ACTION_ENCODED_SIZE
+    for idx, mask_sz in enumerate(UNIT_ACTION_SIZES):
+        mask = pos_mask[m_idx : m_idx + mask_sz]
+        if idx == 0:
+            if not mask[action[idx]]:
+                logging.info(
+                    f"Invalid action type {action[idx]} for unit {unit}. Mask: {mask}"
+                )
+                return False
+        elif idx == 1:
+            if action[0] == 0 and not mask[action[idx]]:
+                logging.info(
+                    f"Invalid move dir {action[idx]} for unit {unit}. Mask: {mask}"
+                )
+                return False
+        elif idx == 2:
+            if action[0] == 1 and not mask[action[idx]]:
+                logging.info(
+                    f"Invalid transfer dir {action[idx]} for unit {unit}. Mask: {mask}"
+                )
+                return False
+        elif idx == 3:
+            if action[0] == 1 and not mask[action[idx]]:
+                logging.info(
+                    f"Invalid transfer resource {action[idx]} for unit {unit}. Mask: {mask}"
+                )
+                return False
+        elif idx == 4:
+            if action[0] == 2 and not mask[action[idx]]:
+                logging.info(
+                    f"Invalid pickup resource {action[idx]} for unit {unit}. Mask: {mask}"
+                )
+                return False
+        m_idx += mask_sz
+    return True
