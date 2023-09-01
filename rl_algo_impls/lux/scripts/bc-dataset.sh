641b70758333d8c2cbef3bf490f4301e3a3abe87
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-09-01 16:36:52 -0700

    Script to download the 5 datasets

---
+++ rl_algo_impls/lux/scripts/bc-dataset.sh
@@ -0,0 +1,21 @@
+mkdir -p data/lux
+cd data/lux
+
+create_dataset() {
+    if [ -d $1 ]; then
+        echo "Dataset $1 already exists"
+        return
+    fi
+    mkdir $1
+    cd $1
+    local kaggle_name="${2:-$1}"
+    kaggle datasets download -d sgoodfriend/$kaggle_name
+    unzip $kaggle_name.zip
+    cd ..
+}
+
+create_dataset lux-replays-deimos-npz
+create_dataset lux-replays-flg-npz
+create_dataset lux-replays-ry_andy_-npz lux-replays-ry-andy-npz
+create_dataset lux-replays-tigga-npz
+create_dataset lux-replays-siestaguru-npz
