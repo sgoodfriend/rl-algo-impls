7f1f625c8d7981d7dcbc66535e1ffbe6fe830d24
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-11-17 21:43:25 -0800

    Revert "If env doesn’t have render_mode, set to rgb_array"
    This reverts commit d0d61e43da3e695584075f52d739357cc057f18b.

--- rl_algo_impls/wrappers/vector_env_render_compat.py
+++ rl_algo_impls/wrappers/vector_env_render_compat.py
@@ -9,10 +9,7 @@ from rl_algo_impls.wrappers.vector_wrapper import VectorEnv, VectorWrapper
 class VectorEnvRenderCompat(VectorWrapper):
     def __init__(self, env: VectorEnv) -> None:
         super().__init__(env)
-        try:
-            self.render_mode = self.env.call("render_mode")[0]  # type: ignore
-        except AttributeError:
-            self.render_mode = "rgb_array"
+        self.render_mode = self.env.call("render_mode")[0]  # type: ignore
 
     def render(self) -> Optional[np.ndarray]:
         imgs = [img for img in self.env.call("render") if img is not None]  # type: ignore
