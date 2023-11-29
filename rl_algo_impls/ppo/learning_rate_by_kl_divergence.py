cb8a7ae5cfaec9d964d998359a1f63eae5ed6e5b
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-11-29 13:59:09 -0800

    LuxAI_S2-v0-j1024env16-80m-vfc003-vlthresh
    Reduce learning rate if v_loss starts increasing

--- rl_algo_impls/ppo/learning_rate_by_kl_divergence.py
+++ rl_algo_impls/ppo/learning_rate_by_kl_divergence.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import numpy as np
 
 from rl_algo_impls.ppo.ppo import PPO, TrainStats
@@ -13,16 +15,29 @@ class LearningRateByKLDivergence(Callback):
         moving_window_size: int = 5,
         max_increase_fraction: float = 1.02,
         min_decrease_fraction: float = 0.5,
+        v_loss_threshold: Optional[float] = None,
+        v_loss_fast_moving_window_size: int = 10,
+        v_loss_slow_moving_window_size: int = 100,
     ) -> None:
         super().__init__()
         self.algo = algo
         self.target_kl = target_kl
-        self.moving_window_size = moving_window_size
         self.max_increase_fraction = max_increase_fraction
         self.min_decrease_fraction = min_decrease_fraction
 
         self.rms_kl = ExponentialMovingMeanVar(window_size=moving_window_size)
-        self.rms_updates = 0
+        self.num_updates = 0
+
+        self.v_loss_threshold = v_loss_threshold
+        if v_loss_threshold is not None:
+            self.slow_v_loss_rms = ExponentialMovingMeanVar(
+                window_size=v_loss_slow_moving_window_size,
+                shape=algo.policy.value_shape,
+            )
+            self.fast_v_loss_rms = ExponentialMovingMeanVar(
+                window_size=v_loss_fast_moving_window_size,
+                shape=algo.policy.value_shape,
+            )
 
     def on_step(
         self, train_stats: TrainStats, timesteps_elapsed: int = 1, **kwargs
@@ -30,14 +45,34 @@ class LearningRateByKLDivergence(Callback):
         super().on_step(timesteps_elapsed)
 
         self.rms_kl.update(np.array([train_stats.approx_kl]))
-        self.rms_updates += 1
+        self.num_updates += 1
 
-        if self.rms_updates > self.moving_window_size:
+        if self.v_loss_threshold is not None:
+            v_loss_array = np.array(train_stats.v_loss)
+            self.slow_v_loss_rms.update(v_loss_array)
+            self.fast_v_loss_rms.update(v_loss_array)
+
+        min_decrease_fraction = self.min_decrease_fraction
+        max_increase_fraction = self.max_increase_fraction
+        if (
+            self.v_loss_threshold is not None
+            and self.num_updates > self.slow_v_loss_rms.window_size
+        ):
+            v_loss_ratio = (
+                self.slow_v_loss_rms.mean.mean() / self.fast_v_loss_rms.mean.mean()
+            )
+            if v_loss_ratio > self.v_loss_threshold:
+                max_increase_fraction -= v_loss_ratio - self.v_loss_threshold
+                max_increase_fraction = max(
+                    max_increase_fraction, min_decrease_fraction
+                )
+
+        if self.num_updates > self.rms_kl.window_size:
             kl = self.rms_kl.mean.item()
             correction_ratio = np.clip(
                 self.target_kl / np.abs(kl),
-                self.min_decrease_fraction,
-                self.max_increase_fraction,
+                min_decrease_fraction,
+                max_increase_fraction,
             )
             self.algo.learning_rate *= correction_ratio
 
