593c2d2e1d631e94bc0b2b5f38b734e848f789be
Scott Goodfriend <goodfriend.scott@gmail.com>
2023-12-13 16:54:54 -0800

    Install vec-noise and jax in poetry context

--- scripts/setup-lux.sh
+++ scripts/setup-lux.sh
@@ -9,7 +9,15 @@
 # chmod 600 ~/.kaggle/kaggle.json
 
 bash ./scripts/setup.sh
-python -m pip install vec-noise
+
+# Activate the Poetry environment
+POETRY_VENV=$(poetry env info -p)
+source "$POETRY_VENV/bin/activate"
+
+# Manually install vec-noise using pip
+pip install vec-noise
+pip install --upgrade "jax[cuda11_cudnn82]==0.4.7" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+
 poetry install -E lux
 
 # kaggle datasets download -d sgoodfriend/lux-replays-flg-npz -p data/lux
