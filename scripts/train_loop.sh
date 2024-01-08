88de5d04621098f7df4c90187b9bc7bb1a4a8d65
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-01-08 09:28:42 -0800

    Add ‘poetry run’ to scripts
    This is so I don’t have to call `poetry shell` for basic operations

--- scripts/train_loop.sh
+++ scripts/train_loop.sh
@@ -18,7 +18,7 @@ train_jobs=""
 job_idx=0
 for env in $(echo $envs); do
     for seed in $seeds; do
-        cmd="python train.py --algo $algo --env $env --seed $seed --pool-size 1 --wandb-tags $WANDB_TAGS --wandb-project-name $project_name --virtual-display"
+        cmd="poetry run python train.py --algo $algo --env $env --seed $seed --pool-size 1 --wandb-tags $WANDB_TAGS --wandb-project-name $project_name --virtual-display"
         if [ "$devices" -gt 1 ] && [ "$job_idx" -lt "$devices" ]; then
             cmd+=" --device-index $job_idx"
         fi
