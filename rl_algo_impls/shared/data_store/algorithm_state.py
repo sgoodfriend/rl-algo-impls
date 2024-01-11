b8385ae8ba975c57bea10185b651ccf2e7765dd0
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-01-11 08:55:22 -0800

    RemoteAlgorithmState sends state to cpu device
    This is to keep it from being transferred to the GPU unnecessary as it goes through the DataStoreActor

--- rl_algo_impls/shared/data_store/algorithm_state.py
+++ rl_algo_impls/shared/data_store/algorithm_state.py
@@ -1,6 +1,6 @@
+import copy
 import logging
 import os
-from typing import Any
 
 import torch
 from torch.optim.optimizer import StateDict
@@ -18,28 +18,39 @@ class SynchronousAlgorithmState(TrackableState):
         return OPTIMIZER_FILENAME
 
     def save(self, path: str) -> None:
-        torch.save(self.get_state(), os.path.join(path, OPTIMIZER_FILENAME))
+        torch.save(self._get_state(), os.path.join(path, OPTIMIZER_FILENAME))
 
     def load(self, path: str) -> None:
         optimizer_path = os.path.join(path, OPTIMIZER_FILENAME)
         if os.path.exists(optimizer_path):
-            self.set_state(torch.load(optimizer_path, map_location=self.algo.device))
+            self._set_state(torch.load(optimizer_path, map_location=self.algo.device))
         else:
             logging.info(
                 f"Optimizer state not found at {optimizer_path}. Not overwriting optimizer state."
             )
 
-    def get_state(self) -> StateDict:
+    def _get_state(self) -> StateDict:
         return self.algo.optimizer.state_dict()
 
-    def set_state(self, state: StateDict) -> None:
+    def _set_state(self, state: StateDict) -> None:
         self.algo.optimizer.load_state_dict(state)
 
 
 class RemoteAlgorithmState(TrackableState):
     def __init__(self, algo: Algorithm) -> None:
-        self.device = algo.device
-        self.state = algo.optimizer.state_dict()
+        cpu_device = torch.device("cpu")
+        orig_state_dict = algo.optimizer.state_dict()
+        dest_state_dict = {}
+        for k, v in orig_state_dict.items():
+            if k == "state":
+                dest_state_dict[k] = {}
+                for k2, v2 in v.items():
+                    dest_state_dict[k][k2] = {}
+                    if torch.is_tensor(v2):
+                        dest_state_dict[k][k2] = v2.to(cpu_device)
+            else:
+                dest_state_dict[k] = copy.copy(v)
+        self.state = dest_state_dict
 
     @property
     def name(self) -> str:
@@ -49,4 +60,4 @@ class RemoteAlgorithmState(TrackableState):
         torch.save(self.state, os.path.join(path, OPTIMIZER_FILENAME))
 
     def load(self, path: str) -> None:
-        self.state = torch.load(path, map_location=self.device)
+        self.state = torch.load(path, map_location=torch.device("cpu"))
