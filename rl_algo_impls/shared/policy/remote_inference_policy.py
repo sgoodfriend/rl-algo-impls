e9e27dac0725b85d2ba0008d921a95ab138dc0f9
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-01-17 15:59:14 -0800

    PolicyWorkerPool is a PolicyActor registrar
    It simply assigns out PolicyActors to RemoteInferencePolicies. It eliminates one jump while generally allowing load to be equally distributed between PolicyActors

--- rl_algo_impls/shared/policy/remote_inference_policy.py
+++ rl_algo_impls/shared/policy/remote_inference_policy.py
@@ -1,4 +1,3 @@
-import logging
 import uuid
 from typing import TYPE_CHECKING, Any, Generic, Optional, TypeVar
 
@@ -6,10 +5,8 @@ import numpy as np
 import ray
 
 from rl_algo_impls.shared.policy.abstract_policy import AbstractPolicy, Step
-from rl_algo_impls.shared.policy.policy_worker_pool import (
-    PolicyArgsRef,
-    PolicyWorkerPool,
-)
+from rl_algo_impls.shared.policy.policy_actor import PolicyActor
+from rl_algo_impls.shared.policy.policy_worker_pool import PolicyWorkerPool
 from rl_algo_impls.wrappers.vector_wrapper import ObsType
 
 if TYPE_CHECKING:
@@ -19,6 +16,9 @@ if TYPE_CHECKING:
 RemoteInferencePolicySelf = TypeVar(
     "RemoteInferencePolicySelf", bound="RemoteInferencePolicy"
 )
+
+_current_actor_id: Optional[str] = None
+_policy_worker_for_current_actor: Optional[PolicyWorker] = None
 
 
 class RemoteInferencePolicy(AbstractPolicy, Generic[ObsType]):
@@ -31,6 +31,8 @@ class RemoteInferencePolicy(AbstractPolicy, Generic[ObsType]):
         self.policy_worker_pool = policy_worker_pool
         self.policy_id = uuid.uuid4().hex
 
+        self.all_actors = ray.get(self.policy_worker_pool.get_all_actors.remote())
+
         _clone_origin = kwargs.get("_clone_origin", None)
         # Private cloning path
         if _clone_origin:
@@ -40,20 +42,36 @@ class RemoteInferencePolicy(AbstractPolicy, Generic[ObsType]):
                 == _clone_origin.policy_worker_pool._actor_id
             ), f"Cannot clone policy from different PolicyWorkerPool"
             ray.get(
-                policy_worker_pool.clone_policy.remote(
-                    _clone_origin.policy_id, self.policy_id
-                )
+                [
+                    actor.clone_policy.remote(_clone_origin.policy_id, self.policy_id)
+                    for actor in self.all_actors
+                ]
             )
         # Public policy path
         else:
             ray.get(
-                policy_worker_pool.add_policy.remote(
-                    PolicyArgsRef(ray.put((self.policy_id, policy)))
-                )
+                [
+                    actor.add_policy.remote(self.policy_id, policy)
+                    for actor in self.all_actors
+                ]
             )
 
     def __repr__(self) -> str:
         return super().__repr__() + f"(policy_id={self.policy_id})"
+
+    @property
+    def assigned_policy_actor(self) -> PolicyActor:
+        global _current_actor_id
+        global _policy_worker_for_current_actor
+        if _current_actor_id is None:
+            _current_actor_id = ray.get_runtime_context().get_actor_id()
+        if _policy_worker_for_current_actor is None:
+            _policy_worker_for_current_actor = ray.get(
+                self.policy_worker_pool.get_policy_for_actor_id.remote(
+                    _current_actor_id
+                )
+            )
+        return _policy_worker_for_current_actor
 
     def act(
         self,
@@ -62,23 +80,21 @@ class RemoteInferencePolicy(AbstractPolicy, Generic[ObsType]):
         action_masks: Optional["NumpyOrDict"] = None,
     ) -> np.ndarray:
         return ray.get(
-            self.policy_worker_pool.act.remote(
-                PolicyArgsRef(
-                    ray.put((self.policy_id, obs, deterministic, action_masks))
-                )
+            self.assigned_policy_actor.act.remote(
+                self.policy_id, obs, deterministic, action_masks
             )
         )
 
     def reset_noise(self) -> None:
-        return self.policy_worker_pool.reset_noise.remote(self.policy_id)
+        for actor in self.all_actors:
+            actor.reset_noise.remote(self.policy_id)
 
     def save(self, path: str) -> None:
-        return self.policy_worker_pool.save.remote(self.policy_id, path)
+        return self.assigned_policy_actor.save.remote(self.policy_id, path)
 
     def set_state(self, state: Any) -> None:
-        self.policy_worker_pool.set_state.remote(
-            PolicyArgsRef(ray.put((self.policy_id, state)))
-        )
+        for actor in self.all_actors:
+            actor.set_state.remote(self.policy_id, state)
 
     def eval(self: RemoteInferencePolicySelf) -> RemoteInferencePolicySelf:
         # No-op because policy is always in eval mode
@@ -91,17 +107,11 @@ class RemoteInferencePolicy(AbstractPolicy, Generic[ObsType]):
         return self
 
     def value(self, obs: ObsType) -> np.ndarray:
-        return ray.get(
-            self.policy_worker_pool.value.remote(
-                PolicyArgsRef(ray.put((self.policy_id, obs)))
-            )
-        )
+        return ray.get(self.assigned_policy_actor.value.remote(self.policy_id, obs))
 
     def step(self, obs: ObsType, action_masks: Optional["NumpyOrDict"] = None) -> Step:
         return ray.get(
-            self.policy_worker_pool.step.remote(
-                PolicyArgsRef(ray.put((self.policy_id, obs, action_masks)))
-            )
+            self.assigned_policy_actor.step.remote(self.policy_id, obs, action_masks)
         )
 
     def logprobs(
@@ -111,8 +121,8 @@ class RemoteInferencePolicy(AbstractPolicy, Generic[ObsType]):
         action_masks: Optional["NumpyOrDict"] = None,
     ) -> np.ndarray:
         return ray.get(
-            self.policy_worker_pool.logprobs.remote(
-                PolicyArgsRef(ray.put((self.policy_id, obs, actions, action_masks)))
+            self.assigned_policy_actor.logprobs.remote(
+                self.policy_id, obs, actions, action_masks
             )
         )
 
