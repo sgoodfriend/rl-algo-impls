06f7146144aff401149fae3fe09b8b6a017f6259
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-01-18 10:59:44 -0800

    Add labels to ray_object_ref printouts

--- debug/ray_object_ref.py
+++ debug/ray_object_ref.py
@@ -4,10 +4,11 @@ ray.init()
 
 
 @ray.remote
-def print_object_ref(object_ref, wrapped_object_ref) -> None:
-    print(object_ref)
-    print(wrapped_object_ref)
+def print_object_ref(object_ref, wrapped_object_ref):
+    print("Automatically dereferenced ObjectRef:", object_ref)
+    print("Still ObjectRef because in tuple:", wrapped_object_ref)
+    return wrapped_object_ref[0]
 
 
 obj_ref = ray.put((1, 2, 3))
-print_object_ref.remote(obj_ref, (obj_ref,))
+print("Returns ObjectRef:", ray.get(print_object_ref.remote(obj_ref, (obj_ref,))))
