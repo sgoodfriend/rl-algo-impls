a6589c6850b0f1078fb2bf5063c978db6ee8a9e8
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-01-18 17:03:49 -0800

    Script to test if possible to load actor in another process

---
+++ debug/multiprocessing_ray.py
@@ -0,0 +1,43 @@
+import ray
+import torch
+
+
+def ray_worker(queue: torch.multiprocessing.Queue):
+    queue.put("start")
+    # Connect to the existing Ray cluster
+    ray.init(address="auto", namespace="avocado")
+    # Now this process can use Ray tasks or actors
+    # ...
+    actor = ray.get_actor("actor")
+    queue.put("actor")
+    queue.put(ray.get(actor.f.remote()))
+    queue.put("f output")
+
+
+@ray.remote
+class Actor:
+    def __init__(self):
+        print("Actor.__init__")
+        self.x = 0
+
+    def f(self):
+        print("Actor.f")
+        self.x += 1
+        return self.x
+
+
+if __name__ == "__main__":
+    # Start Ray in the main process
+    ray.init(
+        namespace="avocado"
+    )  # or ray.init(address='auto') if connecting to a cluster
+
+    # Start a child process that also uses Ray
+    q = torch.multiprocessing.Queue()
+    actor = Actor.options(name="actor").remote()
+    p = torch.multiprocessing.Process(target=ray_worker, args=(q,))
+    p.start()
+
+    while True:
+        s = q.get()
+        print(s)
