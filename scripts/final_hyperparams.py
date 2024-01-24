180d2c2d6db7d8bbe8aa2d53fbea313426de6805
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-01-23 17:59:28 -0800

    DPPO LuxAI_S2-v0-j1024env32-a100-debug

--- scripts/final_hyperparams.py
+++ scripts/final_hyperparams.py
@@ -8,9 +8,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--env-id", type=str)
     parser.add_argument("-a", "--algo", default="appo")
-    parser.set_defaults(
-        env_id="LuxAI_S2-v0-j256env64-80m-ent1-lr01-mgn2-base1lc-ga4-tkl5cl-4rw-2a100"
-    )
+    parser.set_defaults(env_id="LuxAI_S2-v0-j1024env32-a100-debug")
     args = parser.parse_args()
 
     hyperparam_dict = load_hyperparam_dict(args.algo, args.env_id)
