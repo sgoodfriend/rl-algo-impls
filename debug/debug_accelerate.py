c0eb10b98f6f3d2bcf22034f7c1ad1f6a67d1ae1
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-01-25 13:41:26 -0800

    Apparently models don’t have device by default

--- debug/debug_accelerate.py
+++ debug/debug_accelerate.py
@@ -79,8 +79,8 @@ def evaluate(model, eval_loader):
         correct = 0
         total = 0
         for images, labels in eval_loader:
-            images = images.to(model.device)
-            labels = labels.to(model.device)
+            images = images.to("cuda")
+            labels = labels.to("cuda")
             output = model(images)
             _, predicted = torch.max(output.data, 1)
             total += labels.size(0)
