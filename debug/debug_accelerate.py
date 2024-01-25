49d1e87549a36398d3fc7ceeb12a3bea0a53766e
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-01-25 13:10:11 -0800

    Fix the SimpleCNN

--- debug/debug_accelerate.py
+++ debug/debug_accelerate.py
@@ -89,12 +89,12 @@ class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
         # Convolutional layers
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
         # Max pooling
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
         # Fully connected layers
-        self.fc1 = nn.Linear(64 * 7 * 7, 128)
+        self.fc1 = nn.Linear(64 * 12 * 12, 128)
         self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
@@ -102,7 +102,7 @@ class SimpleCNN(nn.Module):
         x = F.relu(self.conv1(x))
         x = self.pool(F.relu(self.conv2(x)))
         # Flatten the output for the fully connected layers
-        x = x.view(-1, 64 * 7 * 7)
+        x = torch.flatten(x, 1)
         # Fully connected layers with ReLU
         x = F.relu(self.fc1(x))
         # Output layer
