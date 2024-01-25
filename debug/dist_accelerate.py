21094261e190adfea6405034eb6cb4d79b3e35c0
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-01-25 15:33:37 -0800

    Move Accelerator() up

--- debug/dist_accelerate.py
+++ debug/dist_accelerate.py
@@ -5,6 +5,9 @@ from accelerate import Accelerator
 
 logging.basicConfig(level=logging.INFO)
 
+accelerator = Accelerator()
+logging.info(f"accelerator.state: {accelerator.state}")
+
 logging.info(f"torch.distributed.is_available(): {torch.distributed.is_available()}")
 logging.info(
     f"torch.distributed.is_initialized(): {torch.distributed.is_initialized()}"
@@ -15,8 +18,6 @@ if torch.distributed.is_initialized():
     logging.info(
         f"torch.distributed.get_world_size(): {torch.distributed.get_world_size()}"
     )
-logging.info(f"torch.cuda.device_count(): {torch.cuda.device_count()}")
 
-accelerator = Accelerator()
-logging.info(f"accelerator.state: {accelerator.state}")
+logging.info(f"torch.cuda.device_count(): {torch.cuda.device_count()}")
 logging.info(f"accelerator.device: {accelerator.device}")
