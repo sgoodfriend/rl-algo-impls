b238a70af805943cde9eb777bf20fd046ea4be79
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-01-25 15:30:00 -0800

    dist_accelerate: What’s available to customize?

---
+++ debug/dist_accelerate.py
@@ -0,0 +1,22 @@
+import logging
+
+import torch
+from accelerate import Accelerator
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info(f"torch.distributed.is_available(): {torch.distributed.is_available()}")
+logging.info(
+    f"torch.distributed.is_initialized(): {torch.distributed.is_initialized()}"
+)
+if torch.distributed.is_initialized():
+    logging.info(f"torch.distributed.get_backend(): {torch.distributed.get_backend()}")
+    logging.info(f"torch.distributed.get_rank(): {torch.distributed.get_rank()}")
+    logging.info(
+        f"torch.distributed.get_world_size(): {torch.distributed.get_world_size()}"
+    )
+logging.info(f"torch.cuda.device_count(): {torch.cuda.device_count()}")
+
+accelerator = Accelerator()
+logging.info(f"accelerator.state: {accelerator.state}")
+logging.info(f"accelerator.device: {accelerator.device}")
