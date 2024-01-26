882d77874bba7dc7a34d7b61aade472fea6874de
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-01-26 13:49:26 -0800

    LuxAI_S2-v0-j512env64-80m-ent1-lr01-mgn2-base1lc-nga-tkl5cl-3r-2a100
    Hope is this will use less memory as the rollouts take longer to accumulate

--- scripts/final_hyperparams.py
+++ scripts/final_hyperparams.py
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--env-id", type=str)
     parser.add_argument("-a", "--algo", default="appo")
-    parser.set_defaults(env_id="LuxAI_S2-v0-j1024env32-a100-debug")
+    parser.set_defaults(
+        env_id="LuxAI_S2-v0-j512env64-80m-ent1-lr01-mgn2-base1lc-nga-tkl5cl-3r-2a100"
+    )
     args = parser.parse_args()
 
     hyperparam_dict = load_hyperparam_dict(args.algo, args.env_id)
