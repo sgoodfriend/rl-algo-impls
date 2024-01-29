2821c106cafd0b827989affe155d1f284326eea1
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-01-29 00:01:02 -0800

    Microrts-env16-80m-ent5-lr3c-mgn2-info-rew-vf50-nga-a100
    Increase vf_coef to 0.5 from 0.24
    
    Also, DPPO Microrts-env16-240m-ent5-lr3c-mgn2-info-rew-vf50-nga-a100

--- scripts/final_hyperparams.py
+++ scripts/final_hyperparams.py
@@ -7,9 +7,9 @@ from rl_algo_impls.runner.running_utils import load_hyperparam_dict
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--env-id", type=str)
-    parser.add_argument("-a", "--algo", default="ppo")
+    parser.add_argument("-a", "--algo", default="appo")
     parser.set_defaults(
-        env_id="Microrts-env16-80m-ent5-lr3c-mgn2-info-rew-vf24-nga-a100"
+        env_id="Microrts-env16-80m-ent5-lr3c-mgn2-info-rew-vf50-nga-a100"
     )
     args = parser.parse_args()
 
