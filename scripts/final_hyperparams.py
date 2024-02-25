44c3c76a5ae3bae970e22af40fdcdbff527926cb
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-02-25 01:39:56 -0800

    Get final hyperparams of DPPO Microrts-env64-240m-ent5d1-lr8d1-05wb2lwr-a100

--- scripts/final_hyperparams.py
+++ scripts/final_hyperparams.py
@@ -7,10 +7,8 @@ from rl_algo_impls.runner.running_utils import load_hyperparam_dict
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--env-id", type=str)
-    parser.add_argument("-a", "--algo", default="ppo")
-    parser.set_defaults(
-        env_id="Microrts-bw16a-20m-ent10d-lr10d01-05wb4lhr-grid2entity-a10"
-    )
+    parser.add_argument("-a", "--algo", default="dppo")
+    parser.set_defaults(env_id="Microrts-env64-240m-ent5d1-lr8d1-05wb2lwr-a100")
     args = parser.parse_args()
 
     hyperparam_dict = load_hyperparam_dict(args.algo, args.env_id)
