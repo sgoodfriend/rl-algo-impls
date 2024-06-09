098d37ba3327147c2c43b81eb9b9969c9f5a317e
Scott Goodfriend <goodfriend.scott@gmail.com>
2024-06-09 00:42:52 -0700

    Microrts-b2w10-grid2entity-ignore-mask-4layers
    Double the depth and width of the transformer backbone to give the model space to represent board positions.

--- rl_algo_impls/train.py
+++ rl_algo_impls/train.py
@@ -51,7 +51,7 @@ def train() -> None:
         # env=["BipedalWalker-v3"],
         # env=["PongNoFrameskip-v4"],
         # env=["BreakoutNoFrameskip-v4"],
-        env=["Microrts-b2w10-grid2entity-ignore-mask"],
+        env=["Microrts-b2w10-grid2entity-ignore-mask-4layers"],
         # env=["Microrts-env32-500m-ent5d1-lr4d1-05wb2lwr-ft-a100"],
         # env=["LuxAI_S2-v0-debug"],
         seed=[1],
